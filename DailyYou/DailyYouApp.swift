c31452a54305a298fd8b6728491f417b05486dd0
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-05-09 13:43:50 +0100

    Initial Commit

---
+++ DailyYou/DailyYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyYouApp.swift
+//  DailyYou
+//
+//  Created by BSTAR on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyYouTests/DailyYouTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyYouTests.swift
+//  DailyYouTests
+//
+//  Created by BSTAR on 09/05/2025.
+//
+
+import Testing
+@testable import DailyYou
+
+struct DailyYouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
